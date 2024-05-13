561f8718da7a2e6f4b1d5ff9b259000c74518b56
ppedvLukasK <lukask@ppedv.de>
2024-05-13 16:52:14 +0200

    M005

--- M005.py
+++ M005.py
@@ -46,12 +46,18 @@ print(["FizzBuzz" if i % 3 == 0 and i % 5 == 0 else
 # Übung 1
 # Schreibe eine List-Comprehension die nur Zahlen aus einer Range von 1 bis inklusive 30 in die neue Liste packt, falls die Zahl durch 6 teilbar ist
 # Bevor die Zahl in die neue Liste gepackt wird, soll sie um 12 erhöht werden
+print([i + 12 for i in range(1, 31) if i % 6 == 0])
 
 # Übung 2
 # Schreibe eine List-Comprehension die aus einem Text alle Kleinbuchstaben nimmt und Groß in die Liste schreibt
+text = "Ich bin ein Text"
+print([z.upper() for z in text if z.islower()])
 
 # Übung 3
 # Schreibe eine List-Comprehension die aus einem Text alle Anfangsbuchstaben nimmt
+print([wort[0] for wort in text.split(" ")])
+print([z for z in text.title() if z.isupper()])
 
 # Übung 4
 # Schreibe eine List-Comprehension die aus einem Text alle Wörter nimmt die 3 oder weniger Zeichen haben
+print([wort for wort in text.split(" ") if len(wort) <= 3])
